e6796c962c998ad93fdbec75aefece54c9e50cc5
Conghui Tan <tanconghui@gmail.com>
2016-12-12 20:13:24 +0800

    example updated

--- example.py
+++ example.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # problem size
     n, d = 1000, 100
 
-    # randomly generate data
+    # randomly generate training data
     A = np.random.randn(n, d)
     x_true = np.random.randn(d)
     y = np.sign(np.dot(A, x_true) + 0.1 * np.random.randn(n))
@@ -30,11 +30,12 @@ if __name__ == '__main__':
     A_test = np.random.randn(n, d)
     y_test = np.sign(np.dot(A_test, x_true))
 
-    # define function and gradient via Theano
+    # preprocess data
     tmp = lil_matrix((n, n))
     tmp.setdiag(y)
     data = theano.shared(tmp * A)
 
+    # define objective function and gradient via Theano
     l2 = 1e-2
     par = T.vector()
     loss = T.log(1 + T.exp(-T.dot(data, par))).mean() + l2 / 2 * (par ** 2).sum()
@@ -44,13 +45,15 @@ if __name__ == '__main__':
     grad = theano.function(inputs=[par, idx], outputs=T.grad(loss, wrt=par),
                            givens={data: data[idx, :]})
 
+    # test SVRG-BB
     x0 = np.random.rand(d)
     print('Begin to run SVRG-BB:')
     x = svrg_bb(grad, 1e-3, n, d, func=func, max_epoch=50)
-    y_pred = np.sign(np.dot(A_test, x))
-    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_pred) / n))
+    y_predict = np.sign(np.dot(A_test, x))
+    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict) / n))
 
+    # test SGD-BB
     print('\nBegin to run SGD-BB:')
     x = sgd_bb(grad, 1e-3, n, d, phi=lambda k: k, func=func, max_epoch=50)
-    y_pred = np.sign(np.dot(A_test, x))
-    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_pred) / n))
+    y_predict = np.sign(np.dot(A_test, x))
+    print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict) / n))
