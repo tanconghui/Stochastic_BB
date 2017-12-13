fb39fa6cbffd0d85da1bad83cdd88d3e0784dded
Conghui Tan <tanconghui@users.noreply.github.com>
2017-12-13 14:12:14 +0800

    Update example.py

--- example.py
+++ example.py
@@ -54,6 +54,6 @@ if __name__ == '__main__':
 
     # test SGD-BB
     print('\nBegin to run SGD-BB:')
-    x = sgd_bb(grad, 1e-3, n, d, phi=lambda k: k, func=func, beta=0.3, max_epoch=50)
+    x = sgd_bb(grad, 1e-3, n, d, phi=lambda k: k, func=func, max_epoch=50)
     y_predict = np.sign(np.dot(A_test, x))
     print('Test accuracy: %f' % (np.count_nonzero(y_test == y_predict)*1.0 / n))
